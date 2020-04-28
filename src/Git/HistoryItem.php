6ef287ccdc3d2a7d9781fe69db682acf53768e58
Stefan Ullrich <su@decaf.de>
2020-04-28 11:02:04 +0200

    chore: variable can be nullable

--- src/Git/HistoryItem.php
+++ src/Git/HistoryItem.php
@@ -5,9 +5,9 @@ namespace Decaf\StandardVersion\Git;
 class HistoryItem
 {
     public string $ref;
-    public string $type;
-    public string $scope;
-    public string $text;
+    public ?string $type;
+    public ?string $scope;
+    public ?string $text;
     public ?string $description = null;
     public bool $isBreakingChange = false;
 }
